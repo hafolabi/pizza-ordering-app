d5ad2b2840feb118aa522447dfd57b4678dae095
Hafolabi <afolabioladipo6@gmail.com>
2022-07-31 07:09:17 +0100

    commit

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -2,31 +2,39 @@ import React, { useState } from "react";
 import { axiosInstance } from "../../config";
 import styles from "../../styles/Product.module.css";
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { addProduct } from '../../redux/cartSlice'
 
-const Product = ({pizza}) => {
+const Product = ({ pizza }) => {
   const [size, setSize] = useState(0);
   const [extras, setExtras] = useState([]);
   const [price, setPrice] = useState(pizza.prices[0]);
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
 
-  const changePrice = (number) =>{
-    setPrice(price + number)
-  }
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
 
-  const handleSize = (sizeIndex) =>{
+  const handleSize = (sizeIndex) => {
     const difference = pizza.prices[sizeIndex] - pizza.prices[size];
     setSize(sizeIndex);
     changePrice(difference);
-  }
+  };
 
-  const handleChange =(e, option) =>{
+  const handleChange = (e, option) => {
     const checked = e.target.checked;
-    if(checked){
-      changePrice(option.price)
-      setExtras((prev)=>[...prev, option])
-    }else{
-      changePrice(-option.price)
-      setExtras(extras.filter((extra)=>(extra._id !== option._id)))
+    if (checked) {
+      changePrice(option.price);
+      setExtras((prev) => [...prev, option]);
+    } else {
+      changePrice(-option.price);
+      setExtras(extras.filter((extra) => extra._id !== option._id));
     }
+  };
+
+  const handleClick =()=>{
+    dispatch(addProduct({...pizza, extras, price, quantity}))
   }
 
   return (
@@ -61,36 +69,42 @@ const Product = ({pizza}) => {
 
         <h3 className={styles.choose}>Choose additional ingredients</h3>
         <div className={styles.ingredients}>
-          {pizza.extraOptions.map(option=>(
-          <div className={styles.option} key={option._id}>
-            <input
-              type="checkbox"
-              id={option.text}
-              name={option.text}
-              className={styles.checkbox}
-              onChange={(e)=>handleChange(e, option)}
-            />
-            <label htmlFor="double">{option.text}</label>
-          </div>
+          {pizza.extraOptions.map((option) => (
+            <div className={styles.option} key={option._id}>
+              <input
+                type="checkbox"
+                id={option.text}
+                name={option.text}
+                className={styles.checkbox}
+                onChange={(e) => handleChange(e, option)}
+              />
+              <label htmlFor="double">{option.text}</label>
+            </div>
           ))}
         </div>
 
         <div className={styles.add}>
-          <input type="number" min={1} defaultValue={1} className={styles.quantity} />
-          <button className={styles.btn}>Add to Cart</button>
+          <input
+            onChange={(e) => setQuantity(e.target.value)}
+            type="number"
+            min={1}
+            defaultValue={1}
+            className={styles.quantity}
+          />
+          <button className={styles.btn} onClick={handleClick}>Add to Cart</button>
         </div>
       </div>
     </div>
   );
 };
 
-export const getServerSideProps = async ({params}) =>{
-  const res = await axiosInstance.get(`/products/${params.id}`)
-  return{
-      props: {
-          pizza: res.data,
-      }
-  }
-}
+export const getServerSideProps = async ({ params }) => {
+  const res = await axiosInstance.get(`/products/${params.id}`);
+  return {
+    props: {
+      pizza: res.data,
+    },
+  };
+};
 
 export default Product;
