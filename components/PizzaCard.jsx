f3396e8a1923052789cda8c245a77058a427e72a
Hafolabi <afolabioladipo6@gmail.com>
2022-08-15 11:34:07 +0100

    commit

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -8,7 +8,9 @@ const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <Link href={`/product/${pizza._id}`} passHref>
-        <Image src={pizza.img} alt='' width='500' height='500' objectFit="cover" />
+        <a>
+          <Image src={pizza.img} alt='' width='500' height='500' objectFit="cover" />
+        </a>
       </Link>
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza?.prices[0]}</span>
